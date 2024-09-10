04d4fea32ee8491874f443d4059836f6040139ab
DaStoopidGuy <dastoopidguy@protonmail.com>
2024-09-10 09:30:36 +0500

    using lazy.nvim as my plugin manager now

--- init.lua
+++ init.lua
@@ -1,29 +1,29 @@
--- Functions for Bootstrapping Paq
-local function clone_paq()
-    local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
-    local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
-    if not is_installed then
-        vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
-        return true
-    end
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-local function bootstrap_paq(packages)
-    local first_install = clone_paq()
-    vim.cmd.packadd("paq-nvim")
-    local paq = require("paq")
-    if first_install then
-        vim.notify("Installing plugins... If prompted, hit Enter to continue.")
-    end
-
-    -- Read and install packages
-    paq(packages)
-    paq.install()
-end
-
+vim.opt.rtp:prepend(lazypath)
 
 -- -----------------------
 -- Options
 -- -----------------------
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local opt = vim.o
 
@@ -63,8 +63,6 @@ opt.scrolloff = 10
 -- Keymaps
 -- -----------------------
 
-vim.g.mapleader = " "
-
 -- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>" , '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
@@ -89,106 +87,115 @@ vim.g.maplocalleader = " "
 -- -----------------------
 -- Plugins
 -- -----------------------
-bootstrap_paq {
-    "savq/paq-nvim", -- Let Paq manage itself
-    { "rose-pine/neovim", as = "rose-pine" },
+require("lazy").setup({
+    {   -- colorscheme
+        "rose-pine/neovim", name = "rose-pine",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            local wk = require('which-key')
+            wk.setup()
+            wk.add({
+                {"<leader>f", group = "[F]ind"},
+                {"<leader>r", group = "[R]ename"},
+                {"<leader>c", group = "[C]ode Actions"},
+            })
+        end
+    },
+    {   -- mini plugins :3
+        "echasnovski/mini.nvim",
+        dependencies = {
+            { 'echasnovski/mini.align'   , branch = "stable", opts = {} },
+            { 'echasnovski/mini.pairs'   , branch = "stable", opts = {} },
+            { 'echasnovski/mini.ai'      , branch = "stable", opts = {} },
+            { 'echasnovski/mini.surround', branch = "stable", opts = {} },
+            {
+                'echasnovski/mini.statusline', branch = "stable",
+                config = function()
+                    local statusline = require('mini.statusline')
+                    statusline.setup({ use_icons = vim.g.have_nerd_font })
+                    statusline.section_location = function()
+                        return "%2l:%-2v"
+                    end
+                end
+            },
+            -- for minimal autocompletion :3
+            { 'echasnovski/mini.completion', branch = "stable", opts = {} },
+            { 'echasnovski/mini-git', name = "mini.git", branch = "stable", opts = {} },
+        },
+    },
+    {   -- telescope config
+        'nvim-telescope/telescope.nvim',
+        priority = 100, -- because lsp config depends upon builtin...
+        dependencies = {
+            { 'nvim-tree/nvim-web-devicons', opts = {} },
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files"})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep"})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers"})
+            vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = "Find recent files"})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find help"})
+            vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Find keymaps"})
+        end
+    },
+    {   -- lsp config
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            { "williamboman/mason.nvim", config = true },
+            { "williamboman/mason-lspconfig.nvim", opts = { ensure_installed = { "lua_ls" } }},
+        },
+        config = function()
+            local builtin = require("telescope.builtin")
+            local on_attach = function(_, _)
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename        , { desc = "Rename symbol" })
+                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action   , { desc = "Code Actions" })
+                vim.keymap.set('n', 'gd'        , vim.lsp.buf.definition    , { desc = "Goto Definition" })
+                vim.keymap.set('n', 'gi'        , vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+                vim.keymap.set('n', 'gr'        , builtin.lsp_references    , { desc = "Goto References" })
+                vim.keymap.set('n', 'K'         , vim.lsp.buf.hover         , { desc = "Show documentation" })
+            end
 
-    "folke/which-key.nvim",
+            local lspconfig = require("lspconfig")
+            local handlers = {
+                function (server_name) -- default handler
+                    lspconfig[server_name].setup { on_attach = on_attach }
+                end,
 
-    {'echasnovski/mini.nvim', branch = "stable"},
-    {'echasnovski/mini.align', branch = "stable"},
-    {'echasnovski/mini.pairs', branch = "stable"},
-    {'echasnovski/mini.ai', branch = "stable"},
-    {'echasnovski/mini.surround', branch = "stable"},
-    {'echasnovski/mini.statusline', branch = "stable"},
-    {'echasnovski/mini.completion', branch = "stable"},
-
-    'nvim-tree/nvim-web-devicons',
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-
-    "akinsho/toggleterm.nvim",
-}
--- setting up plugins
-vim.cmd("colorscheme rose-pine")
-
--- which-key config
-local wk = require('which-key')
-wk.setup()
-wk.add({
-    {"<leader>f", group = "[F]ind"},
-    {"<leader>r", group = "[R]ename"},
-    {"<leader>c", group = "[C]ode Actions"},
+                -- targeted overrides for specific servers
+                ["lua_ls"] = function()
+                    lspconfig.lua_ls.setup({
+                        on_attach = on_attach,
+                        settings = { Lua = { diagnostics = { globals = {"vim"}, } } }
+                    })
+                end
+            }
+            require("mason-lspconfig").setup_handlers(handlers)
+        end
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup()
+            vim.keymap.set("n", "<C-x>", function()
+                vim.cmd("ToggleTerm")
+            end)
+            vim.keymap.set("t", "<C-x>", function()
+                vim.cmd("ToggleTerm")
+            end)
+        end
+    },
 })
-
--- mini config
-require('mini.align').setup()
-require('mini.pairs').setup()
-require('mini.ai').setup()
-require('mini.surround').setup()
-local statusline = require('mini.statusline')
-statusline.setup({ use_icons = vim.g.have_nerd_font })
-statusline.section_location = function()
-    return "%2l:%-2v"
-end
-require('mini.completion').setup()
-
--- telescope config
-require('nvim-web-devicons').setup()
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files"})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep"})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers"})
-vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = "Find recent files"})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find help"})
-vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Find keymaps"})
-
--- lsp config
-local lspconfig = require("lspconfig")
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls" }
-})
-local on_attach = function(_, _)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename        , { desc = "Rename symbol" })
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action   , { desc = "Code Actions" })
-    vim.keymap.set('n', 'gd'        , vim.lsp.buf.definition    , { desc = "Goto Definition" })
-    vim.keymap.set('n', 'gi'        , vim.lsp.buf.implementation, { desc = "Goto Implementation" })
-    vim.keymap.set('n', 'gr'        , builtin.lsp_references    , { desc = "Goto References" })
-    vim.keymap.set('n', 'K'         , vim.lsp.buf.hover         , { desc = "Show documentation" })
-end
-
-local handlers = {
-    function (server_name) -- default handler
-        lspconfig[server_name].setup { on_attach = on_attach }
-    end,
-
-    -- targeted overrides for specific servers
-    ["lua_ls"] = function()
-        lspconfig.lua_ls.setup({
-            on_attach = on_attach,
-            settings = { Lua = { diagnostics = { globals = {"vim"}, } } }
-        })
-    end
-}
-require("mason-lspconfig").setup_handlers(handlers)
-
--- toggleterm config
-require("toggleterm").setup()
-vim.keymap.set("n", "<C-x>", function()
-    vim.cmd("ToggleTerm")
-end)
-vim.keymap.set("t", "<C-x>", function()
-    vim.cmd("ToggleTerm")
-end)
-
 
 -- neovide specific config >:3
 if vim.g.neovide then
     vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
 end
+
